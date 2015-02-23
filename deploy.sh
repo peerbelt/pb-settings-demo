f6fe527b21c26b8614865ce7e0dfc56be0610caf
DD <dpd_damyanov@abv.bg>
2015-02-23 10:06:41 +0200

    @HL#L

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,9 @@ EB_BUCKET=peerbelt
 DOCKERRUN_FILE=Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws s3 cp Dockerfile s3://$EB_BUCKET/Dockerfile
 aws elasticbeanstalk create-environment  --application-name LB-AS-peer-conf --environment-name TEST-$CIRCLE_BUILD_NUM --template-name peer-elb-as
-aws elasticbeanstalk create-application-version --application-name LB-AS-peer-conf --version-label $SHA1 --source-bundle S3Bucket=peerbelt,S3Key=Dockerrun.aws.json
+aws elasticbeanstalk create-application-version --application-name LB-AS-peer-conf --version-label $SHA1 --source-bundle S3Bucket=peerbelt,S3Key=Dockerfile
 #Create environtment takes a while, tha is why the below loop is necessary. Without it, the build will fail. 
          COUNTER=0
          while [  $COUNTER -lt 10 ]; do
